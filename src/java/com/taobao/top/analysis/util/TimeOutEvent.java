8a62f9a52b1e5a05c7b0277d0af8a7da6e1661bc
fangweng <cenwenchu@163.com>
2011-12-06 13:29:39 +0800

    update

--- src/java/com/taobao/top/analysis/util/TimeOutEvent.java
+++ src/java/com/taobao/top/analysis/util/TimeOutEvent.java
@@ -4,6 +4,7 @@
 package com.taobao.top.analysis.util;
 
 /**
+ * 超时队列中放置的数据
  * @author fangweng
  * @email: fangweng@taobao.com
  * 2011-12-5 下午1:51:35
@@ -11,10 +12,18 @@ package com.taobao.top.analysis.util;
  */
 public interface TimeOutEvent extends java.lang.Comparable<TimeOutEvent> {
 	
+	/**
+	 * 获得创建时间，long类型，毫秒
+	 * @return
+	 */
 	public long getEventCreateTime();
 	
 	public void setEventCreateTime(long eventCreateTime);
 	
+	/**
+	 * 当前事件最大有效期，单位秒
+	 * @return
+	 */
 	public long getMaxEventHoldTime();
 	
 	public void setMaxEventHoldTime(long maxEventHoldTime);
